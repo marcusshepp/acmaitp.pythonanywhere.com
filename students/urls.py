d8a76f9dceea33f132910007086d25ed305dbd45
sheph2mj <sheph2mj@cmich.edu>
2015-08-31 14:31:21 -0400

    fix url bug

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from .views import CreateStudent
 
 urlpatterns = [
-    url(r'signup/^', CreateStudent.as_view()),
+    url(r'^signup', CreateStudent.as_view()),
 ]
