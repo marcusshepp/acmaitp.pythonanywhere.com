76d8b42ce03a2f25aa6d8925b40e9efda30d6c7c
sheph2mj <sheph2mj@cmich.edu>
2015-09-02 13:34:13 -0400

    still messing with template dirs

--- acm/settings.py
+++ acm/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'acm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
