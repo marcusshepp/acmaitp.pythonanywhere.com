63829c5e621bdb96f427c81b31f8b4b2a028e0ad
marcusshepp <sheph2mj@cmich.edu>
2015-12-02 21:26:48 -0500

    register techtalk model in admin

--- students/admin.py
+++ students/admin.py
@@ -4,8 +4,10 @@ from .models import (
     Student,
     Officer,
     Position
+    TechTalk
 )
 
 admin.site.register(Student)
 admin.site.register(Officer)
 admin.site.register(Position)
+admin.site.register(TechTalk)
